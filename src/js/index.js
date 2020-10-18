ff41bdf0dd301eab2294679c62f0bf056101d28c
jc-alcaldev <carlos_juan1812@hotmail.com>
2020-10-18 18:42:20 +0000

    Random Card Terminado

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,15 @@ import "../style/index.scss";
 const suits = ["\u25ca", "\u2665", "\u2660", "\u2663"];
 const number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-// let trebol = "u2660";
+window.onload = function() {
+  randomCard();
+};
+
 let myButton = document.querySelector("#myBtn");
 
 myButton.addEventListener("click", function() {
   randomCard();
 });
-
-// window.onload = function() {
-//   randomCard();
-// };
 
 function randomCard() {
   let suit = Math.floor(Math.random() * suits.length);
